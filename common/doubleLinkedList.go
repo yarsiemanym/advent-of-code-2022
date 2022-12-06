5c2e140b67544007435164eadb9e0d0ed64b8d68
Ray Saltrelli <ray@saltrelli.com>
2022-12-05 23:17:40 -0500

    Better DoubleLinkedList

--- common/doubleLinkedList.go
+++ common/doubleLinkedList.go
@@ -1,21 +1,24 @@
 package common
 
+import "log"
+
 type DoubleLinkedListNode struct {
 	value    interface{}
 	next     *DoubleLinkedListNode
 	previous *DoubleLinkedListNode
+	list     *DoubleLinkedList
 }
 
-func (node *DoubleLinkedListNode) Next() *DoubleLinkedListNode {
-	return node.next
+func (thisNode *DoubleLinkedListNode) Next() *DoubleLinkedListNode {
+	return thisNode.next
 }
 
-func (node *DoubleLinkedListNode) Previous() *DoubleLinkedListNode {
-	return node.previous
+func (thisNode *DoubleLinkedListNode) Previous() *DoubleLinkedListNode {
+	return thisNode.previous
 }
 
-func (node *DoubleLinkedListNode) Value() interface{} {
-	return node.value
+func (thisNode *DoubleLinkedListNode) Value() interface{} {
+	return thisNode.value
 }
 
 type DoubleLinkedList struct {
@@ -28,29 +31,104 @@ func (list *DoubleLinkedList) Length() int {
 	return list.length
 }
 
-func (list *DoubleLinkedList) Head() *DoubleLinkedListNode {
-	return list.head
+func (thisList *DoubleLinkedList) Head() *DoubleLinkedListNode {
+	return thisList.head
 }
 
-func (list *DoubleLinkedList) Tail() *DoubleLinkedListNode {
-	return list.tail
+func (thisList *DoubleLinkedList) Tail() *DoubleLinkedListNode {
+	return thisList.tail
 }
 
-func (list *DoubleLinkedList) Append(value interface{}) {
+func (thisList *DoubleLinkedList) Append(value interface{}) {
 	node := &DoubleLinkedListNode{
 		value: value,
+		list:  thisList,
 	}
 
-	if list.head == nil {
-		list.head = node
-		list.tail = node
+	if thisList.head == nil {
+		thisList.head = node
+		thisList.tail = node
 	} else {
-		node.previous = list.tail
-		list.tail.next = node
-		list.tail = node
+		node.previous = thisList.tail
+		thisList.tail.next = node
+		thisList.tail = node
 	}
 
-	list.length++
+	thisList.length++
+}
+
+func (thisList *DoubleLinkedList) Prepend(value interface{}) {
+	newNode := &DoubleLinkedListNode{
+		value: value,
+		list:  thisList,
+	}
+
+	if thisList.head == nil {
+		thisList.head = newNode
+		thisList.tail = newNode
+	} else {
+		newNode.next = thisList.head
+		thisList.head.previous = newNode
+		thisList.head = newNode
+	}
+
+	thisList.length++
+}
+
+func (thisList *DoubleLinkedList) InsertAfter(value interface{}, thisNode *DoubleLinkedListNode) {
+	nextNode := thisNode.next
+	newNode := &DoubleLinkedListNode{
+		value:    value,
+		next:     nextNode,
+		previous: thisNode,
+		list:     thisList,
+	}
+	thisNode.next = newNode
+	nextNode.previous = newNode
+
+	if newNode.next == nil {
+		thisList.tail = newNode
+	}
+
+	thisList.length++
+}
+
+func (thisList *DoubleLinkedList) InsertBefore(value interface{}, thisNode *DoubleLinkedListNode) {
+	previousNode := thisNode.previous
+	newNode := &DoubleLinkedListNode{
+		value:    value,
+		next:     thisNode,
+		previous: previousNode,
+		list:     thisList,
+	}
+	thisNode.previous = newNode
+	previousNode.next = newNode
+
+	if newNode.previous == nil {
+		thisList.head = newNode
+	}
+
+	thisList.length++
+}
+
+func (thisList *DoubleLinkedList) Remove(thisNode *DoubleLinkedListNode) {
+	if thisNode.list != thisList {
+		log.Panic("The specified DoubleLinkedListNode is not an element of this DoubleLinkedList.")
+	}
+
+	if thisNode.previous == nil {
+		thisList.head = thisNode.next
+	} else {
+		thisNode.previous.next = thisNode.next
+	}
+
+	if thisNode.next == nil {
+		thisList.tail = thisNode.previous
+	} else {
+		thisNode.next.previous = thisNode.previous
+	}
+
+	thisList.length--
 }
 
 func NewDoubleLinkedList() *DoubleLinkedList {
