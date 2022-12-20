940a5c6c4114315fb104e8cb8ced0d6cddcdefad
Ray Saltrelli <ray@saltrelli.com>
2022-12-20 16:35:52 -0500

    Add cursor manipulation

--- vt100/cursor.go
+++ vt100/cursor.go
@@ -10,6 +10,22 @@ func MoveCursorTo(x int, y int) {
 	fmt.Printf("\x1b[%d;%df", y, x)
 }
 
+func MoveCursorUp(y int) {
+	fmt.Printf("\x1b[%dA", y)
+}
+
+func MoveCursorDown(y int) {
+	fmt.Printf("\x1b[%dB", y)
+}
+
+func MoveCursorRight(x int) {
+	fmt.Printf("\x1b[%dC", x)
+}
+
+func MoveCursorLeft(x int) {
+	fmt.Printf("\x1b[%dD", x)
+}
+
 func SaveCursorPosition() {
 	fmt.Print("\x1b[s")
 }
